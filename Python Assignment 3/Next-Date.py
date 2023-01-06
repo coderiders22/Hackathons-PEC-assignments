f72d3789c0d49262a03a79b281d995b61c34b6b4
coderiders22 <119496654+coderiders22@users.noreply.github.com>
2023-01-06 15:08:51 +0530

    Python Assignments

--- Python Assignment 3/Next-Date.py
+++ Python Assignment 3/Next-Date.py
@@ -17,36 +17,63 @@
 # Created:     11-12-2022
 #-------------------------------------------------------------------------------
 
-
 date = int(input("Enter date: "))
 month = int(input("Enter month: "))
 year = int(input("Enter year: "))
 
 oddMonths = [1, 3, 5, 7, 8, 10, 12]
 
-if date >= 1 and month >= 1 and year >= 1800:
-    if date <= 31 and month <= 12 and year <= 2025:
-        date += 1
 
-if date > 28 and month == 2:
-    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
-        date = 1
-        month += 1
-
-if date > 29 and month == 2:
-    date = 1
-    month += 1
-
-elif date > 30 and oddMonths.index(month) is not int:
-    date = 1
-    month += 1
-
-elif date > 31:
-    date = 1
-    month += 1
-    if month == 12:
+if month in oddMonths:
+    if month == 12 and date == 31:
         date = 1
         month = 1
         year += 1
+    else:
+        if date < 31:
+            date += 1
+        elif date == 31:
+            date = 1
+            month += 1
+
+elif month not in oddMonths:
+
+    if month != 2:
+        if date == 30:
+            date = 1
+            month += 1
+        elif date < 30:
+            date += 1
+
+    elif month == 2:
+        
+        if date < 28:
+            date += 1
+
+        elif date >= 28:
+
+            leap_year = False
+        
+            if year % 4 == 0:
+                if year % 100 == 0:
+                    if year % 400 == 0:
+                        leap_year = True
+                    else:
+                        leap_year = False
+                else:
+                    leap_year = True
+            else:
+                leap_year = False
+
+            if leap_year:
+                if date == 29:
+                    date = 1
+                    month += 1
+        
+            elif not leap_year:
+                if date == 28:
+                    date = 1
+                    month += 1
+
 
 print("Next date is: ", date, "/", month, "/", year)
