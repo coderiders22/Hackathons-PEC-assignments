128c438ff89b9f7676926e7d4673681f62ad99d4
coderiders22 <119496654+coderiders22@users.noreply.github.com>
2023-01-27 18:58:19 +0530

    Python Assignments

--- Python Assignment 6/Sum-zero.py
+++ Python Assignment 6/Sum-zero.py
@@ -7,24 +7,28 @@
 #              Output: [[-10, 2, 8], [-7, -3, 10]]
 # Question No. : 8
 # Created:     20-01-2023
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
+class py_solution:
+ def threeSum(self, nums):
+        nums, result, i = sorted(nums), [], 0
+        while i < len(nums) - 2:
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] < 0:
+                    j += 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                else:
+                    result.append([nums[i], nums[j], nums[k]])
+                    j, k = j + 1, k - 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+            i += 1
+            while i < len(nums) - 2 and nums[i] == nums[i - 1]:
+                i += 1
+        return result
 
-input_array = [-25, -10, -7, -3, 2, 4, 8, 10]
-
-class set_of_number:
-
-    def __init__(self, _input_array):
-        output_array = []
-
-        for i in _input_array:
-            for j in _input_array:
-                for k in _input_array:
-                    if i + j + k == 0:
-                        _ijklist = [i, j, k]
-                        output_array.append(_ijklist)
-
-        print(output_array)
-
-
-set1 = set_of_number(input_array)
+print(py_solution().threeSum([-25, -10, -7, -3, 2, 4, 8, 10]))
